9697b0e89aacd2f45c2e6eae17bdc30885347320
Leon <yanglyu.leon.7@gmail.com>
2018-11-02 11:49:09 -0700

    start: Tutorial of BullsEye
    https://www.raywenderlich.com/5993-your-first-ios-app

---
+++ TutorialBullsEye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TutorialBullsEye
+//
+//  Created by Yang Lu on 2018-11-02.
+//  Copyright © 2018 IdiotLeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
